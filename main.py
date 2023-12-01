e7ca65a800ba4e21e6a945f32d1ce486bddb86f3
ahmed <Ahmedrayan.jaaf@gmail.com>
2023-12-01 08:21:33 +0100

    Joan -_-

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from fonction import *
 
 # Call of the function
-directory = "C:/Users/joanp/PycharmProjects/pychatbot-Jaafar-Oyono--PMP1-/speeches"
+# directory = "C:/Users/joanp/PycharmProjects/pychatbot-Jaafar-Oyono--PMP1-/speeches"
+directory = "speeches"
 files_names = list_of_files(directory, "txt")
 print(files_names)
 # list_names(files_names)
