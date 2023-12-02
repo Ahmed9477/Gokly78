7aa946488055506107893db5c1e0540b51622e67
ahmed <Ahmedrayan.jaaf@gmail.com>
2023-12-02 07:12:02 +0100

    Joan -_-

--- main.py
+++ main.py
@@ -3,9 +3,15 @@ from fonction import *
 # Call of the function
 # directory = "C:/Users/joanp/PycharmProjects/pychatbot-Jaafar-Oyono--PMP1-/speeches"
 directory = "speeches"
-files_names = list_of_files(directory, "txt")
-print(files_names)
-# list_names(files_names)
-# lowercase(files_names)
 
+
+def list_of_files(directory, param):
+    pass
+
+
+files_names = list_of_files(directory, "txt")
+
+list_names("speeches")
+#lowercase(files_names)
+print(list(Poureviterdoublage))
 list_names("speeches")
